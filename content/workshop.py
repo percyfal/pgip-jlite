ae011325cfd8dd577bb2464a48d261e65488b3a9
Per Unneberg <per.unneberg@scilifelab.se>
2023-06-22 14:13:02 +0200

    Add more exercise drafts

--- content/workshop.py
+++ content/workshop.py
@@ -1,7 +1,75 @@
+from datetime import datetime
+
 import tqdm
-import tskit
+import yaml
 from IPython.core.display import HTML
 from jupyterquiz import display_quiz
+
+
+def load_quiz(section):
+    with open("quiz.yaml", "r") as fh:
+        try:
+            quiz = yaml.safe_load(fh)
+        except yaml.YAMLError as e:
+            print(e)
+    if section in quiz.keys():
+        return quiz[section]
+    return
+
+
+scilife_colors = {
+    "lime": "#a7c947",
+    "lime25": "#e9f2d1",
+    "lime50": "#d3e4a3",
+    "lime75": "#bdd775",
+    "teal": "#045c64",
+    "teal25": "#c0d6d8",
+    "teal50": "#82aeb2",
+    "teal75": "#43858b",
+    "aqua": "#4c979f",
+    "aqua25": "#d2e5e7",
+    "aqua50": "#a6cbcf",
+    "aqua75": "#79b1b7",
+    "grape": "#491f53",
+    "grape25": "#d2c7d4",
+    "grape50": "#a48fa9",
+    "grape75": "#77577e",
+    "lightgray": "#e5e5e5",
+    "mediumgray": "#a6a6a6",
+    "darkgray": "#3f3f3f",
+    "black": "#202020",
+}
+
+color_dict = {
+    "--jq-multiple-choice-bg": scilife_colors[
+        "lime"
+    ],  # Background for the question part of multiple-choice questions
+    "--jq-mc-button-bg": scilife_colors[
+        "teal25"
+    ],  # Background for the buttons when not pressed
+    "--jq-mc-button-border": scilife_colors["teal50"],  # Border of the buttons
+    "--jq-mc-button-inset-shadow": scilife_colors[
+        "teal75"
+    ],  # Color of inset shadow for pressed buttons
+    "--jq-many-choice-bg": scilife_colors[
+        "teal25"
+    ],  # Background for question part of many-choice questions
+    "--jq-numeric-bg": scilife_colors[
+        "lime"
+    ],  # Background for question part of numeric questions
+    "--jq-numeric-input-bg": scilife_colors[
+        "lime75"
+    ],  # Background for input area of numeric questions
+    "--jq-numeric-input-label": scilife_colors[
+        "lime"
+    ],  # Color for input of numeric questions
+    "--jq-numeric-input-shadow": scilife_colors[
+        "lime75"
+    ],  # Color for shadow of input area of numeric questions when selected
+    "--jq-incorrect-color": scilife_colors["grape"],  # Color for incorrect answers
+    "--jq-correct-color": scilife_colors["teal50"],  # Color for correct answers
+    "--jq-text-color": scilife_colors["lime25"],  # Color for question text
+}
 
 
 class DownloadProgressBar(tqdm.tqdm):
@@ -12,12 +80,8 @@ class DownloadProgressBar(tqdm.tqdm):
 
 
 class Workbook:
-    css = """<style>
-        dl {border: green 1px solid; margin-top: 1em}
-        dt {color: white; background-color: green; padding: 4px; display: block; }
-        dd {padding: 4px;}
-    </style>"""
-
+    styles = open("./styles/custom.css", "r").read()
+    css = f"<style>{styles}</style>"
     # See https://github.com/jupyterlite/jupyterlite/issues/407#issuecomment-1353088447
     ready_text = """
     <table style="width: 100%;"><tr>
@@ -68,15 +132,12 @@ class Workbook:
         "font-weight:normal;transform:rotate(90deg);text-anchor:start;dominant-baseline:central;}"  # noqa
     )
 
-    # some useful functions
-    @staticmethod
-    def convert_metadata_to_new_format(ts):
-        # Quick hack to read individual and population metadata as a python dict
-        tables = ts.dump_tables()
-        tables.populations.metadata_schema = tskit.MetadataSchema.permissive_json()
-        tables.individuals.metadata_schema = tskit.MetadataSchema.permissive_json()
-        tables.sites.metadata_schema = tskit.MetadataSchema.permissive_json()
-        return tables.tree_sequence()
+    def __init__(self):
+        name = type(self).__name__
+        self.quiz = load_quiz(name)
+
+    def question(self, label):
+        display_quiz(self.quiz[label], colors=color_dict)
 
     @staticmethod
     def download(url):
@@ -89,73 +150,29 @@ class Workbook:
         return HTML(self.css + self.ready_text + self.reset_button)
 
 
+class HOWTO(Workbook):
+    def __init__(self):
+        super().__init__()
+        self.quiz["Q2"][0]["answers"][0]["value"] = datetime.today().day
+
+
 class WrightFisher(Workbook):
     def __init__(self):
-        pass
+        super().__init__()
 
-    def Q1(self):
-        display_quiz(
-            [
-                {
-                    "question": "How many edges in this tree sequence?",
-                    "type": "numeric",
-                    "answers": [
-                        {
-                            "type": "value",
-                            "value": 10,
-                            "correct": True,
-                            "feedback": "Correct.",
-                        },
-                        {
-                            "type": "range",
-                            "range": [-100000000, 100000],
-                            "correct": False,
-                            "feedback": "Try again (hint: look at `ts.num_edges()`)",
-                        },
-                    ],
-                },
-                {
-                    "question": "How many sites in this tree sequence?",
-                    "type": "numeric",
-                    "answers": [
-                        {
-                            "type": "value",
-                            "value": 10,
-                            "correct": True,
-                            "feedback": "Correct.",
-                        },
-                        {
-                            "type": "range",
-                            "range": [-100000000, 100000],
-                            "correct": False,
-                            "feedback": "Try again (hint: look at `ts.num_sites()`)",
-                        },
-                    ],
-                },
-                {
-                    "question": "How many mutations in this tree sequence?",
-                    "type": "numeric",
-                    "answers": [
-                        {
-                            "type": "value",
-                            "value": 10,
-                            "correct": True,
-                            "feedback": "Correct: there may be more mutations than "
-                            "sites because there could be multiple mutations at a "
-                            "single site",
-                        },
-                        {
-                            "type": "range",
-                            "range": [-100000000, 100000],
-                            "correct": False,
-                            "feedback": "Try again (hint: look at "
-                            "`ts.num_mutations()`)",
-                        },
-                    ],
-                },
-            ]
-        )
+
+class CoalescentHandson(Workbook):
+    def __init__(self):
+        super().__init__()
+
+
+def setup_coalescent_handson():
+    return CoalescentHandson()
 
 
 def setup_wright_fisher():
     return WrightFisher()
+
+
+def setup_howto():
+    return HOWTO()
